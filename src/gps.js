d0d9ab8956b7e7fbb95628b8349dd9020de06db3
Mikael_Nilsson_Enfo <mikael.nilsson@enfo.se>
2020-06-04 21:28:40 +0200

    add actual file

--- src/gps.js
+++ src/gps.js
@@ -6,9 +6,11 @@ let position = {};
 function getLocation() {
     console.log('2. searching position');
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, errorFn);
-        console.log('4. i getLocation, efter showposition', position);
-        return position;
+        return new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(showPosition, errorFn);
+        });
+        
+        
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
@@ -19,9 +21,14 @@ function showPosition(pos) {
     position = pos;
     x.innerHTML = "Latitude: " + pos.coords.latitude +
     "<br>Longitude: " + pos.coords.longitude;
+
+    resolve(pos);
+
 }
 
 function errorFn(err) {
     console.error('an error occurred', err);
     x.innerHTML = `CHAOS! ${err}`;
+
+    reject(err);
 }
