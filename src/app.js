fe77bbaeda851dc6327316edcb7172102b7d1b1a
Mikael_Nilsson_Enfo <mikael.nilsson@enfo.se>
2020-06-04 21:28:26 +0200

    make promise of getCurrentPosition

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ var app = new Vue({
     el: '#app',
     template: `
         <div>
-            <!--<a-scene vr-mode-ui="enabled: false" embedded arjs="sourceType: webcam; debugUIEnabled: false;">-->
+            <a-scene vr-mode-ui="enabled: false" embedded arjs="sourceType: webcam; debugUIEnabled: false;">
                 <camview></camview>
-            <!--</a-scene>-->
+            </a-scene>
             <div id="loc"></div>
         </div>
     `,
