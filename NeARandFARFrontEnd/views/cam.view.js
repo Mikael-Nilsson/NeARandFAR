9cc7564476b282f19fdf1d4b3278c8b3a0aac236
Mikael_Nilsson_Enfo <mikael.nilsson@enfo.se>
2020-08-05 17:17:37 +0200

    Add text at user pos

--- NeARandFARFrontEnd/views/cam.view.js
+++ NeARandFARFrontEnd/views/cam.view.js
@@ -10,7 +10,27 @@ Vue.component('camview', {
         assets: async function() {
             const d = await getData();
             console.log('data collected', d.length);
-            this.assetArray = d;
+         
+            
+            d.push({
+              geometry: 'text',
+              scale: '40 40 40',
+              value: 'Am I close?',
+              position: {
+                lat: this.position.latitude + 0.001,
+                lon: this.position.longitude
+              }
+            });
+            
+            this.assetArray = d.map(p => {
+             return {
+              geometry: p.geometry,
+              value: p.value,
+              scale: p.scale,
+              position: p.position,
+              entityPos: `latitude: ${p.position.latitude}; longitude: ${p.position.longitude}`
+             }
+            });
         }
     },
     computed: {
@@ -36,11 +56,19 @@ Vue.component('camview', {
            assetArray: []
         }
     },
-    
+    /*
+    Kanske skapa aframe-scenen och peta in i en iframe a la
+    <iframe></iframe>
+
+document.querySelector('iframe')
+        .contentDocument.write("<h1>Injected from parent frame</h1>")
+    */
     template: `
         <div>
             <template v-if="loading">
                 <div>loading...</div>
+                <div>{{pos}}</div>
+                <div>{{assetArray}}</div>
             </template>
             <template v-if="!loading">
                 <a-scene ar vr-mode-ui="enabled: false" embedded arjs="sourceType: webcam; debugUIEnabled: false;">
@@ -61,3 +89,6 @@ Vue.component('camview', {
         </div>
     `
 });
+
+
+
