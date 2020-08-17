d942f6455f229235ef86a45a2d432936d0a7272e
Mikael_Nilsson_Enfo <mikael.nilsson@enfo.se>
2020-08-17 13:53:47 +0200

    Better handling of body in checkRequest

--- NeARandFARBackEnd/src/Utilities/RequestUtil.cs
+++ NeARandFARBackEnd/src/Utilities/RequestUtil.cs
@@ -1,4 +1,7 @@
 using System.Collections.Generic;
+using System.Text.Json;
+
+using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
 
 namespace NeARandFARBackEnd
@@ -7,20 +10,25 @@ namespace NeARandFARBackEnd
     {
         public Dictionary<string, string> checkRequest(APIGatewayProxyRequest request, string[] values) {
 
+            LambdaLogger.Log(System.Text.Json.JsonSerializer.Serialize(request, typeof(APIGatewayProxyRequest)).ToString());
+
             Dictionary<string, string> result = new Dictionary<string, string>();
+            Dictionary<string, string> body = new Dictionary<string, string>();
+            if(request.Body != null) {
+                body = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, string>>(request.Body);
+            }
             
             foreach(string val in values) {
-                if(request.QueryStringParameters != null && request.QueryStringParameters["collection"] != null) {
-                    result[val] = request.QueryStringParameters["collection"];
+                LambdaLogger.Log($"looking for parameter {val}");
 
-                } else if(request.PathParameters != null && request.PathParameters["collection"] != null) {
-                    result[val] = request.PathParameters["collection"];
+                if(request.QueryStringParameters != null && request.QueryStringParameters[val] != null) {
+                    result[val] = request.QueryStringParameters[val];
+
+                } else if(request.PathParameters != null && request.PathParameters[val] != null) {
+                    result[val] = request.PathParameters[val];
                 
-                } else if (request.Body != null) {
-                    Dictionary<string, string> body = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, string>>(request.Body);
-                    if(body["collection"] != null) {
-                        result[val] = body["collection"];
-                    }
+                } else if (request.Body != null && body.ContainsKey(val) && body[val] != null) {
+                    result[val] = body[val];
                 }
             }
             
