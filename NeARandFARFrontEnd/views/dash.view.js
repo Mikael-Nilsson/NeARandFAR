1186232f93929281053403472bc5a8d20088de75
Mikael_Nilsson_Enfo <mikael.nilsson@enfo.se>
2020-08-21 08:42:29 +0200

    Create some kind of dashboard and a non-working logout

--- NeARandFARFrontEnd/views/dash.view.js
+++ NeARandFARFrontEnd/views/dash.view.js
@@ -1,5 +1,24 @@
 const dashView = Vue.component('dashview', {
+    mounted: function() {
+        console.log('mounting dashboard');
+    },
+    methods: {
+        logout: function() {
+            console.log('logging out');
+            localStorage.removeItem('sessionActive');
+            localStorage.removeItem('apiKey');
+            this.$router.push({path: '/#/'});
+        }
+    },
+    data: function() {
+        return {
+            pos: gpsService.position.coords
+        };
+    },
     template: `
-    <div>dashboard</div>
+    <div>
+        <div>{{pos.latitude}}, {{pos.longitude}}</div>
+        <button id="logout" v-on:click="logout()">logout</button>
+    </div>
     `
 });
