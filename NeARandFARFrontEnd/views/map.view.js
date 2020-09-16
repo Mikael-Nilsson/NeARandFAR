45f12e4641ed9bd25b5bd82f60bbfddddf0985a5
Mikael_Nilsson <micek@hotmail.com>
2020-09-16 09:23:02 +0200

    Starting to implement map

--- NeARandFARFrontEnd/views/map.view.js
+++ NeARandFARFrontEnd/views/map.view.js
@@ -3,7 +3,25 @@ console.log('initializing map');
 const mapView = Vue.component('mapview', {
     created: function() {
     },
+    mounted: function () {
+        this.map = L.map('map').setView([38.63, -90.23], 12); // TODO: Use actual GPS
+
+        this.tileLayer = L.tileLayer(
+            'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
+            {
+                maxZoom: 18,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+            }
+        ); this.tileLayer.addTo(this.map);
+    },
+    data: function () {
+        return {
+            map: null,
+            tileLayer: null,
+            layers: []
+        };
+    },
     template: `
-    <div>map</div>
+    <div id="map"></div>
     `
 });
